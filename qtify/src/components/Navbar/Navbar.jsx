2e912cf431d55222ce33d03667fe6a17417c8620
Bhargav Parashar <bhargavparashar99@gmail.com>
2024-09-27 01:33:26 +0530

    milestone 4.2

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Button from "../Button/Button";
 import Logo from "../Logo/Logo";
 import Search from "../Search/Search";
@@ -8,9 +7,6 @@ import styles from "./Navbar.module.css";
 function Navbar({ searchData }) {
   return (
     <nav className={styles.navbar}>
-      {/* <Link to="/">
-        <Logo />
-      </Link> */}
       <div>
         <Logo />
       </div>
